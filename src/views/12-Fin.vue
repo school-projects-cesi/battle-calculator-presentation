010cd8647cdf7216b259ea4d2224ffb5888f2912
Kristen Jestin <kris.jestin@laposte.net>
2021-02-13 19:35:24 +0100

    feat: début ajout de la dernière slide

---
+++ src/views/12-Fin.vue
@@ -0,0 +1,13 @@
+12
+<template>
+	<section>
+		<h3>a vous de jouer</h3>
+		<b class="compte-rebours">5min</b>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Sommaire',
+}
+</script>
